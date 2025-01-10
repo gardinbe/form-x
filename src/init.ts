becbf6ac16db67d2a0b31e015e93a6563bd93ba4
Ben Gardiner <ben.gardiner@appius.com>
2025-01-10 14:43:05 +0000

    fix unused input type check

--- src/init.ts
+++ src/init.ts
@@ -1,4 +1,4 @@
-import type { ControlElement, MultiControlElement } from './control/control';
+import type { ControlElement } from './control/control';
 import { Control } from './control/control';
 import type { FormValidatorElement } from './form/form-validator';
 import { FormValidator } from './form/form-validator';
@@ -8,27 +8,17 @@ const init = async () => {
   await domReady;
 
   const controlEls = document
-    .querySelectorAll('[fx-validate]');
+    .querySelectorAll<ControlElement>('[fx-validate]');
 
   for (const el of controlEls) {
-    if (
-      'type' in el
-      && (
-        el.type === 'checkbox'
-        || el.type === 'radio'
-      )
-    ) {
-      new Control(el as MultiControlElement);
-    } else {
-      new Control(el as ControlElement);
-    }
+    new Control(el);
   }
 
   const formEls = document
-    .querySelectorAll('[fx-form]');
+    .querySelectorAll<FormValidatorElement>('[fx-form]');
 
   for (const el of formEls) {
-    new FormValidator(el as FormValidatorElement);
+    new FormValidator(el);
   }
 };
 
